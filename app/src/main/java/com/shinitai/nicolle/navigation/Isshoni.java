8520a1b79a5c2593c0ec2ba503cd7c73cb2e7785
Nikkisenpai <rock.me.nm@hotmail.com>
2016-12-06 06:10:30 -0200

    Navigation
    Navigation

---
+++ app/src/main/java/com/shinitai/nicolle/navigation/Isshoni.java
@@ -0,0 +1,19 @@
+package com.shinitai.nicolle.navigation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Avell B155 on 03/12/2016.
+ */
+public class Isshoni extends android.support.v4.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.isshoni,container,false);
+    }
+}
